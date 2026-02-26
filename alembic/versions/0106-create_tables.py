afc43a2c30eb03108928f87f7725d1576ea46c2d
Michał Albrycht <malbrycht@starfishstorage.com>
2026-02-26 13:47:25 +0100

    Example migrations

---
+++ alembic/versions/0106-create_tables.py
@@ -0,0 +1,55 @@
+# ***********************************************************************************************************
+#
+# Starfish Storage Corporation ("Starfish") CONFIDENTIAL
+# Unpublished Copyright (c) 2011 - present Starfish Storage Corporation, All Rights Reserved.
+#
+# NOTICE: This file and its contents (1) constitute Starfish's "External Code" under Starfish's most-recent
+# Limited Software End-User License Agreement, and (2) is and remains the property of Starfish. The
+# intellectual and technical concepts contained herein are proprietary to Starfish and may be covered by
+# U.S. and/or foreign patents or patents in process, and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material is strictly forbidden unless prior
+# written permission is obtained from Starfish. Access to the source code contained herein is hereby
+# forbidden to anyone except (A) current Starfish employees, managers, or contractors who have executed
+# confidentiality or nondisclosure agreements explicitly covering such access, and (B) licensees of
+# Starfish's software.
+#
+# ANY REPRODUCTION, COPYING, MODIFICATION, DISTRIBUTION, PUBLIC PERFORMANCE, OR PUBLIC DISPLAY OF OR
+# THROUGH USE OF THIS SOURCE CODE WITHOUT THE EXPRESS WRITTEN CONSENT OF STARFISH IS STRICTLY PROHIBITED
+# AND IS IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES. THE RECEIPT OR POSSESSION OF THIS
+# FILE OR ITS CONTENTS AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE,
+# DISCLOSE, OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT MAY DESCRIBE, IN
+# WHOLE OR IN PART.
+#
+# FOR U.S. GOVERNMENT CUSTOMERS REGARDING THIS DOCUMENTATION/SOFTWARE
+#   These notices shall be marked on any reproduction of this data, in whole or in part.
+#   NOTICE: Notwithstanding any other lease or license that may pertain to, or accompany the delivery of,
+#   this computer software, the rights of the Government regarding its use, reproduction and disclosure are
+#   as set forth in Section 52.227-19 of the FARS Computer Software-Restricted Rights clause.
+#   RESTRICTED RIGHTS NOTICE: Use, duplication, or disclosure by the Government is subject to the
+#   restrictions as set forth in subparagraph (c)(1)(ii) of the Rights in Technical Data and Computer
+#   Software clause at DFARS 52.227-7013.
+#
+# ***********************************************************************************************************
+"""
+Create partitioned tables
+"""
+
+import os
+import sqlparse
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "0106"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    file_name = os.path.basename(__file__)
+    migration_file_name = str(file_name.rsplit(".", 1)[0]) + ".sql"
+    sql_file = os.path.join(os.path.dirname(__file__), migration_file_name)
+    with open(sql_file) as f:
+        sql_stmts = f.read()
+    op.execute(sqlparse.format(sql_stmts, strip_comments=True))
---
+++ alembic/versions/0109-disable_autoanalyze_on_calculated_aggrs.py
@@ -0,0 +1,52 @@
+# ***********************************************************************************************************
+#
+# Starfish Storage Corporation ("Starfish") CONFIDENTIAL
+# Unpublished Copyright (c) 2011 - present Starfish Storage Corporation, All Rights Reserved.
+#
+# NOTICE: This file and its contents (1) constitute Starfish's "External Code" under Starfish's most-recent
+# Limited Software End-User License Agreement, and (2) is and remains the property of Starfish. The
+# intellectual and technical concepts contained herein are proprietary to Starfish and may be covered by
+# U.S. and/or foreign patents or patents in process, and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material is strictly forbidden unless prior
+# written permission is obtained from Starfish. Access to the source code contained herein is hereby
+# forbidden to anyone except (A) current Starfish employees, managers, or contractors who have executed
+# confidentiality or nondisclosure agreements explicitly covering such access, and (B) licensees of
+# Starfish's software.
+#
+# ANY REPRODUCTION, COPYING, MODIFICATION, DISTRIBUTION, PUBLIC PERFORMANCE, OR PUBLIC DISPLAY OF OR
+# THROUGH USE OF THIS SOURCE CODE WITHOUT THE EXPRESS WRITTEN CONSENT OF STARFISH IS STRICTLY PROHIBITED
+# AND IS IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES. THE RECEIPT OR POSSESSION OF THIS
+# FILE OR ITS CONTENTS AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE,
+# DISCLOSE, OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT MAY DESCRIBE, IN
+# WHOLE OR IN PART.
+#
+# FOR U.S. GOVERNMENT CUSTOMERS REGARDING THIS DOCUMENTATION/SOFTWARE
+#   These notices shall be marked on any reproduction of this data, in whole or in part.
+#   NOTICE: Notwithstanding any other lease or license that may pertain to, or accompany the delivery of,
+#   this computer software, the rights of the Government regarding its use, reproduction and disclosure are
+#   as set forth in Section 52.227-19 of the FARS Computer Software-Restricted Rights clause.
+#   RESTRICTED RIGHTS NOTICE: Use, duplication, or disclosure by the Government is subject to the
+#   restrictions as set forth in subparagraph (c)(1)(ii) of the Rights in Technical Data and Computer
+#   Software clause at DFARS 52.227-7013.
+#
+# ***********************************************************************************************************
+"""
+Disable autoanalyze on sf.calculated_aggrs
+"""
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "0109"
+down_revision = "0108"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # This should disable autoanalyze on sf.calculated_aggrs.
+    # This is the fix for PG 13, which introduced feature that autoanalyze is run even if only inserts are performed.
+    # Number `1000000000` is big enough to disable autoanalyze.
+    # We suspect that autoanalyze conflicts with plain ANALYZE, because we have seen errors in aggregator thread,
+    # which can not be explained otherwise.
+    op.execute("ALTER TABLE sf.calculated_aggrs SET (autovacuum_analyze_threshold = 1000000000);")
